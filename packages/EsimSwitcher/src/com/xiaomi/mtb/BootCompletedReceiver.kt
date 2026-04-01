75ae3e04d6409270231c5df6ee1a449ef85bc4bf
raebaexxx <vadimfeda@yandex.ru>
2026-04-01 03:02:29 +0300

    Revert "EsimSwitcher: Switch to non_removable_euicc_slots to check for eUICC"
    This reverts commit b1a83691236ab65f4c6ca6ec2a53acd6ac8f5ab1.

--- packages/EsimSwitcher/src/com/xiaomi/mtb/BootCompletedReceiver.kt
+++ packages/EsimSwitcher/src/com/xiaomi/mtb/BootCompletedReceiver.kt
@@ -23,19 +23,13 @@ class BootCompletedReceiver : BroadcastReceiver() {
     override fun onReceive(context: Context, intent: Intent) {
         if (DEBUG) Log.d(TAG, "Received boot completed intent: ${intent.action}")
         if (intent.action == Intent.ACTION_BOOT_COMPLETED) {
-            val hasNonRemovableEuicc =
-                context.resources
-                    .getIntArray(com.android.internal.R.array.non_removable_euicc_slots)
-                    .isNotEmpty()
-            Log.i(TAG, "eSIM supported: $hasNonRemovableEuicc")
+            val hasEuiccFeature =
+                context.packageManager.hasSystemFeature(PackageManager.FEATURE_TELEPHONY_EUICC)
+            Log.i(TAG, "eSIM supported: $hasEuiccFeature")
 
-            setComponentEnabled(
-                context,
-                EsimSettingsActivity::class.java.name,
-                hasNonRemovableEuicc,
-            )
+            setComponentEnabled(context, EsimSettingsActivity::class.java.name, hasEuiccFeature)
 
-            if (hasNonRemovableEuicc) {
+            if (hasEuiccFeature) {
                 EsimController.getInstance(context).onBootCompleted()
             }
         }
